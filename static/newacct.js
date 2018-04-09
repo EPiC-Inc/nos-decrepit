5b7b54037955eddd13451ff90ac3664e180e18d9
[R3dact3d] <emmettwb@gmail.com>
2018-04-09 12:51:46 -0600

    Update newacct.js

--- static/newacct.js
+++ static/newacct.js
@@ -1,1 +1,54 @@
+// Dependencies
+var socket = io();
 
+// Variables
+var usr = document.getElementById("username");
+var pwd = document.getElementById("password");
+var username = '';
+var password = '';
+
+// Functions
+hashCode = function(str){
+    var hash = 0;
+    if (str.length == 0) return hash;
+    for (i = 0; i < str.length; i++) {
+        char = str.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function newAcct() {
+  username = usr.value;
+  password = hashCode(pwd.value);
+  console.log("test");
+  socket.emit('new user', [username, password]);
+}
+
+// Callbacks
+socket.on('err', function(data){
+  document.getElementById("errors").innerHTML = data;
+  console.log('err : '+data);
+  pwd.value='';
+});
+
+socket.on('a-ok', function(){
+  window.location.replace("/static/coms.html");
+});
