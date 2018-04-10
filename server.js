c817f5eee152178daf5ebbbb182ac8026e6b0522
[R3dact3d] <emmettwb@gmail.com>
2018-04-10 15:57:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -59,13 +59,13 @@ io.on('connection', function(socket){
     var uData = authList[user];
     //console.log(data);
     //console.log(uData);
-    if (uData == undefined || uData.pass !== pwd) {
-      io.to(socket.id).emit('err', "Error: Username / password not recognized");
-    } else if (!uData.active) {
+    if (!uData.active) {
       io.to(socket.id).emit('err', "Error: You have been banned!");
+    } else if (uData == undefined || uData.pass !== pwd) {
+      io.to(socket.id).emit('err', "Error: Username / password not recognized");
     } else {
       io.to(socket.id).emit('a-ok');
-    }
+    } 
   });
 
   // Message sending script allowing for username colors
