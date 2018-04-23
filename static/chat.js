38f8d3c0993f55bc7f20b7430597b48e719286be
[R3dact3d] <emmettwb@gmail.com>
2018-04-23 13:37:35 -0600

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -142,7 +142,7 @@ socket.on('a-ok', function(){
 socket.on('users online', function(data){
   users.innerHTML = '';
   for (i in data) {
-    users.innerHTML += '<br><button style="background:lightcyan;border:none;font-size:15px;" onclick="m.value+=\'@'+data[i]+'\'">'+data[i]+'</button>';
+    users.innerHTML += '<br><div class="userbtn"><button style="background:lightcyan;border:none;font-size:15px;" onclick="m.value+=\'@'+data[i]+'\'">'+data[i]+'</button></div>';
   }
 });
 
