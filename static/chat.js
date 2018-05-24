e20ef31bdee034791f88bb0ef7995b889b420be8
[R3dact3d] <emmettwb@gmail.com>
2018-05-24 13:48:12 -0600

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -79,7 +79,7 @@ function logout() {
 
 function sendMsg() {
   if (room !== undefined && m.value.trim() !== '') {
-    socket.emit('message', btoa(m.value));
+    socket.emit('message', Buffer.from(m.value).toString('base64'));
     m.value = '';
   }
 }
@@ -155,7 +155,7 @@ socket.on('users online', function(data){
 
 socket.on("message", function(data){
   console.log(data);
-  data = atob(String(data));
+  data = Buffer.from(data, 'base64').toString('utf8')
   // Add message
   var start='<div>'
   if (!alertWaiting) {
