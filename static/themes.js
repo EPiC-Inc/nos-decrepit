0fa6cf1b730c592589c0505c6e4d09b530f3ab31
Fred18295 <42583354+Fred18295@users.noreply.github.com>
2018-08-30 11:00:48 -0600

    fixed issues with theme changes

--- static/themes.js
+++ static/themes.js
@@ -44,35 +44,37 @@ function changecss(theClass, element, value) {
 // theme setters are here as to not include junk in coms.html
 
 
-
+//Making a new theme? Ask Fred18295 for some info first so you don't break anything
 function darkTheme() {
-    document.cookie='theme=dark';
-    changecss('body','background-color','rgb(13, 13, 13)');
-    changecss('header','background-color', 'rgb(77, 77, 77)');
-    changecss('#menu', 'background-color', 'rgb(77, 77, 77)');
-    changecss('#menu', 'color', 'rgb(217, 217, 217)');
-    changecss('.userbtn', 'background-color', 'rgb(140, 140, 140)');
-    changecss('button', 'background-color', 'rgb(140, 140, 140)');
-    changecss('button', 'border', '2px solid rgb(166, 166, 166)');
-    changecss('h1','color','rgb(217, 217, 217)');
-    changecss('#messages','color','rgb(179, 179, 179)');
-    changecss('.msg', 'color', 'rgb(191, 191, 191)');
-    changecss('.msgSender', 'background-color', 'rgb(77, 77, 77)');
-    changecss('.dropbtn', 'background-color', 'rgb(140, 140, 140)');
-    changecss('span', 'color', 'white');
-    changecss('input', 'background-color', 'rgb(128, 128, 128)');
+  document.cookie='theme=dark';
+  defaultTheme();
+  changecss('body','background-color','rgb(13, 13, 13)');
+  changecss('header','background-color', 'rgb(77, 77, 77)');
+  changecss('#menu', 'background-color', 'rgb(77, 77, 77)');
+  changecss('#menu', 'color', 'rgb(217, 217, 217)');
+  changecss('.userbtn', 'background-color', 'rgb(140, 140, 140)');
+  changecss('button', 'background-color', 'rgb(140, 140, 140)');
+  changecss('button', 'border', '2px solid rgb(166, 166, 166)');
+  changecss('h1','color','rgb(217, 217, 217)');
+  changecss('#messages','color','rgb(179, 179, 179)');
+  changecss('.msg', 'color', 'rgb(191, 191, 191)');
+  changecss('.msgSender', 'background-color', 'rgb(77, 77, 77)');
+  changecss('.dropbtn', 'background-color', 'rgb(140, 140, 140)');
+  changecss('span', 'color', 'white');
+  changecss('input', 'background-color', 'rgb(128, 128, 128)');
 }
 function minimalDarkTheme() {
   document.cookie='theme=minimalDark';
+  defaultTheme();
   changecss('body','background-color','black');
   changecss('header','background-color', 'black');
   changecss('#menu', 'background-color', 'black');
   changecss('#menu', 'color', 'white');
   changecss('.userbtn', 'background-color', 'black');
-  changecss('.userbtn','color','white');
+  changecss('.userbtn','color','white'); //NID
   changecss('button', 'background-color', 'black');
   changecss('button', 'border', '2px solid white');
-  changecss('button','color','white');
+  changecss('button','color','white'); //NID
   changecss('h1','color','white');
   changecss('#messages','color','white');
   changecss('.msg', 'color', 'white');
@@ -80,29 +82,30 @@ function minimalDarkTheme() {
   changecss('.dropbtn', 'background-color', 'black');
   changecss('span', 'color', 'black');
   changecss('input', 'background-color', 'black');
-  changecss('.msgSender', 'color', 'rgb(0, 0, 0)');
-  changecss('.userbtn', 'border', '2px solid white');
-  changecss('.msgSender button','border','2px solid white');
-  changecss('.msgSender input','color','white');
-  changecss('.msgSender input','border','2px solid white');
-  changecss('input','border','2px solid white');
+  changecss('.msgSender', 'color', 'rgb(0, 0, 0)'); //NID
+  changecss('.userbtn', 'border', '2px solid white'); //NID
+  changecss('.msgSender button','border','2px solid white'); //NID
+  changecss('.msgSender input','color','white'); //NID
+  changecss('.msgSender input','border','2px solid white'); //NID
+  changecss('input','border','2px solid white'); //NID
 }
 function painTheme() {
-    document.cookie='theme=pain';
-    changecss('body','background-color','rgb(56, 16, 16)'); //
-    changecss('header','background-color', 'rgb(136, 133, 0)'); //
-    changecss('#menu', 'background-color', 'rgb(39, 97, 64)'); //
-    changecss('#menu', 'color', 'rgb(255, 153, 0)'); //
-    changecss('.userbtn', 'background-color', 'rgb(204, 0, 204)'); //
-    changecss('button', 'background-color', 'rgb(142, 62, 92)'); //
-    changecss('button', 'border', '2px solid rgb(210, 255, 228)'); //
-    changecss('h1','color','rgb(64, 65, 41)'); //
-    changecss('#messages','color','rgb(153, 102, 51)'); //
-    changecss('.msg', 'color', 'rgb(191, 191, 191)'); //
-    changecss('.msgSender', 'background-color', 'rgb(13, 191, 66)'); //
-    changecss('.dropbtn', 'background-color', 'rgb(105, 255, 204)'); //
-    changecss('span', 'color', 'white'); //
-    changecss('input', 'background-color', 'rgb(153, 153, 102)'); //
+  document.cookie='theme=pain';
+  defaultTheme();
+  changecss('body','background-color','rgb(56, 16, 16)'); //
+  changecss('header','background-color', 'rgb(136, 133, 0)'); //
+  changecss('#menu', 'background-color', 'rgb(39, 97, 64)'); //
+  changecss('#menu', 'color', 'rgb(255, 153, 0)'); //
+  changecss('.userbtn', 'background-color', 'rgb(204, 0, 204)'); //
+  changecss('button', 'background-color', 'rgb(142, 62, 92)'); //
+  changecss('button', 'border', '2px solid rgb(210, 255, 228)'); //
+  changecss('h1','color','rgb(64, 65, 41)'); //
+  changecss('#messages','color','rgb(153, 102, 51)'); //
+  changecss('.msg', 'color', 'rgb(191, 191, 191)'); //
+  changecss('.msgSender', 'background-color', 'rgb(13, 191, 66)'); //
+  changecss('.dropbtn', 'background-color', 'rgb(105, 255, 204)'); //
+  changecss('span', 'color', 'white'); //
+  changecss('input', 'background-color', 'rgb(153, 153, 102)'); //
 }
 function defaultTheme() {
   document.cookie='theme=default';
@@ -120,4 +123,12 @@ function defaultTheme() {
   changecss('.dropbtn', 'background-color', '#3498DB');
   changecss('span', 'color', 'black');
   changecss('input', 'background-color', 'white');
+  //Fix for Min Dark THeme
+  chagecss('.userbtn', 'color', 'black');
+  changecss('button', 'color', 'black')
+  changecss('.msgSender', 'color', 'black');
+  changecss('.userbtn', 'border', '2px solid black');
+  changecss('.msgSender input','color','black');
+  changecss('.msgSender input','border','2px solid black');
+  changecss('input','border','2px solid black');
 }
