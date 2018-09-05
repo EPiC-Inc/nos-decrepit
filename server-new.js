bc8e1ef71cea9b1ae6ad4d086d3da68f1066ff78
[R3dact3d] <emmettwb@gmail.com>
2018-09-05 14:27:05 -0600

    Update server-new.js

--- server-new.js
+++ server-new.js
@@ -1,1 +1,18 @@
 // Refactored version of server.js
+
+// Dependencies - don't need to worry too much about these
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var url = require('url');
+var fs = require('fs');
+var sanitize = require('sanitize-html');
+var stdin = process.openStdin();
+// End dependencies
+
+// Vars
+var commands = require('./commands.js');
+var users = {};
+var authList = require('./users.json');
+var rooms = require('./rooms.json');
